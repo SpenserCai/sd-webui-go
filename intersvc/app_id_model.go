90daa90644714f847d91a8ed4481aa6a9a9e4e74
SpenserCai <spensercai@gmail.com>
2023-08-14 20:30:50 +0800

    support app_id queue_status sdapi_v1_progress

--- intersvc/app_id_model.go
+++ intersvc/app_id_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-14 20:18:13
+ * @Description: file content
+ */
 package intersvc
 
-import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
-)
+// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 
 type AppIdResponse struct {
-
+	AppId string `json:"app_id"`
 }
--- intersvc/queue_status_model.go
+++ intersvc/queue_status_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-14 20:22:57
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type QueueStatusResponse struct {
-
-}
+type QueueStatusResponse = SdApiModel.Estimation
