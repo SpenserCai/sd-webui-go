0241746325d450f5d6cc01d3a655b5c2b2929b77
SpenserCai <spensercai@gmail.com>
2023-08-14 22:08:47 +0800

    support sd-models

--- intersvc/sdapi_v1_sd_models_model.go
+++ intersvc/sdapi_v1_sd_models_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-14 22:01:10
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type SdapiV1SdModelsResponse struct {
-
-}
+type SdapiV1SdModelsResponse = []SdApiModel.SDModelItem
