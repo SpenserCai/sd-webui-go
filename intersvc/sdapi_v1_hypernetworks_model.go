2eb0f0c46ca7edb0e968f7605912cfd156d56abc
SpenserCai <spensercai@gmail.com>
2023-08-14 23:07:39 +0800

    support:embeddings,samplers,vae,upscalers,hypernetworks

--- intersvc/sdapi_v1_hypernetworks_model.go
+++ intersvc/sdapi_v1_hypernetworks_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-14 23:06:32
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type SdapiV1HypernetworksResponse struct {
-
-}
+type SdapiV1HypernetworksResponse = []SdApiModel.HypernetworkItem
--- intersvc/sdapi_v1_upscalers_model.go
+++ intersvc/sdapi_v1_upscalers_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-14 22:54:02
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type SdapiV1UpscalersResponse struct {
-
-}
+type SdapiV1UpscalersResponse = []SdApiModel.UpscalerItem
