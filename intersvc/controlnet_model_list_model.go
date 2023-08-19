15b55ff25c2ce6860831cc21e69e982ee703cbbc
SpenserCai <spensercai@gmail.com>
2023-08-20 04:18:24 +0800

    support all controlnet api

--- intersvc/controlnet_model_list_model.go
+++ intersvc/controlnet_model_list_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-20 04:03:19
+ * @Description: file content
+ */
 package intersvc
 
-import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
-)
+// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 
 type ControlnetModelListResponse struct {
-
+	ModelList []string `json:"model_list"`
 }
--- intersvc/controlnet_version_model.go
+++ intersvc/controlnet_version_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-20 04:00:35
+ * @Description: file content
+ */
 package intersvc
 
-import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
-)
+// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 
 type ControlnetVersionResponse struct {
-
+	Version int `json:"version"`
 }
