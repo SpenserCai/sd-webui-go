026a76a0a5611423b48ab7dd34f129b465e91727
SpenserCai <spensercai@gmail.com>
2023-08-21 19:00:30 +0800

    support face_restore memory script_info script

--- intersvc/sdapi_v1_face_restorers_model.go
+++ intersvc/sdapi_v1_face_restorers_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-21 18:44:35
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type SdapiV1FaceRestorersResponse struct {
-
-}
+type SdapiV1FaceRestorersResponse = []SdApiModel.FaceRestorerItem
--- intersvc/sdapi_v1_scripts_model.go
+++ intersvc/sdapi_v1_scripts_model.go
@@ -1,9 +1,15 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-08-21 18:48:24
+ * @Description: file content
+ */
 package intersvc
 
 import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
+	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
-type SdapiV1ScriptsResponse struct {
-
-}
+type SdapiV1ScriptsResponse = SdApiModel.ScriptsList
