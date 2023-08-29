a343782019ef89562390ef365cbbb9cee0068fbd
SpenserCai <spensercai@gmail.com>
2023-08-29 21:59:44 +0800

    update check

--- intersvc/sdapi_v1_upscalers_model.go
+++ intersvc/sdapi_v1_upscalers_model.go
@@ -3,7 +3,7 @@
  * @Date: 2023-08-14 17:36:46
  * @version:
  * @LastEditors: SpenserCai
- * @LastEditTime: 2023-08-14 22:54:02
+ * @LastEditTime: 2023-08-29 21:14:04
  * @Description: file content
  */
 package intersvc
@@ -12,4 +12,5 @@ import (
 	SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 )
 
+// Checked: True
 type SdapiV1UpscalersResponse = []SdApiModel.UpscalerItem
