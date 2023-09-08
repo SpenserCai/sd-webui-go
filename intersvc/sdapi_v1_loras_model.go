c16d24073ff2be1646472b145ec3e2f8a10aba29
SpenserCai <spensercai@gmail.com>
2023-09-08 15:16:22 +0800

    support lora info

--- intersvc/sdapi_v1_loras_model.go
+++ intersvc/sdapi_v1_loras_model.go
@@ -1,9 +1,25 @@
+/*
+ * @Author: SpenserCai
+ * @Date: 2023-08-14 17:36:46
+ * @version:
+ * @LastEditors: SpenserCai
+ * @LastEditTime: 2023-09-08 15:14:59
+ * @Description: file content
+ */
 package intersvc
 
-import (
-	// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
-)
+// SdApiModel "github.com/SpenserCai/sd-webui-go/stablediffusion/models"
 
-type SdapiV1LorasResponse struct {
-
+type LoraItemMetadata struct {
+	SsSdModelName      string `json:"ss_sd_model_name"`
+	SsBaseModelVersion string `json:"ss_base_model_version"`
 }
+
+type LoraItem struct {
+	Name     string           `json:"name"`
+	Alias    string           `json:"alias"`
+	Path     string           `json:"path"`
+	Metadata LoraItemMetadata `json:"metadata"`
+}
+
+type SdapiV1LorasResponse = []LoraItem
